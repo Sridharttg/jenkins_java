7975164ff900f40e38b544e08fefe37733fd2d0a
unknown <abcsoftwareacademy@gmail.com>
2021-10-06 17:49:39 +0200

    ttg

---
+++ src/main/java/com/ttg/app/App.java
@@ -0,0 +1,21 @@
+package com.ttg.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Hello World ttg!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
